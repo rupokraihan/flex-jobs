0bcc6e1f5011b57d8f14dd5e5c840285d1d01a60
rupokraihan <rupok2321@gmail.com>
2023-04-10 22:56:07 +0600

    Manrope font added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import MyImage from '../assets/P3OLGJ1 copy 1.png'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className="my-container flex flex-col items-center justify-between lg:flex-row">
+      <div>
+        <h1 className="text-[80px] font-extrabold">
+          One Step
+          <br /> Closer To Your <br /> <span className='text-purple-500'>Dream Job</span>
+        </h1>
+        <p>
+          Explore thousands of job opportunities with all the information you
+          need. Its your future. Come find it. Manage all your job application
+          from start to finish.
+        </p>
+        <button className="my-button">Get Started</button>
+      </div>
+      <div>
+        <img src={MyImage} alt="" />
+      </div>
     </div>
   );
 };
